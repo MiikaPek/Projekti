c732cdc1b82c59486d6d030af5cfb1c70d92df28
MiikaPek <miika.pekkarinen@seamk.fi>
2022-09-27 00:21:01 +0300

    illan mähinät

--- public/front.js
+++ public/front.js
@@ -1,5 +1,5 @@
 // Luo taulukon kirjoista etusivulle
-function readCustomer(){
+function readCustomer() {
   var xmlhttp = new XMLHttpRequest();
   xmlhttp.open("GET", "/customers",true);
   xmlhttp.send();
@@ -35,82 +35,82 @@ function readCustomer(){
    }
    document.getElementById("demo").appendChild(table);
 
-      }
   }
+}
 // Etusivun taulukon luonnissa kutsutaan funktiota, ettei tarvi kirjoittaa auki jokaiseen solun luontiin erikseen.
 function createCell(value) {
-let newCell = document.createElement('td');
-newCell.innerHTML = value;
-return newCell;
+  let newCell = document.createElement('td');
+  newCell.innerHTML = value;
+  return newCell;
 }
   }
   readCustomer();
 
 // Luo päivitys, lisäys ja poistoformit
 function createForm(customer, action) {
-let newCell = document.createElement('td');
-let form = document.createElement('form');
-form.method = (action == 'delete') ? 'POST' : 'GET';
+  let newCell = document.createElement('td');
+  let form = document.createElement('form');
+  form.method = (action == 'delete') ? 'POST' : 'GET';
 
-// Ternääri (ternatry) operaatio, ensimmäinen vaihtoehto true ja jälkimmäinen false. Vertaa IF
-form.action = (action == 'delete') ? '/deleteCustomer' : '/updateCustomer.html';
+  // Ternääri (ternatry) operaatio, ensimmäinen vaihtoehto true ja jälkimmäinen false. Vertaa IF
+  form.action = (action == 'delete') ? '/deleteCustomer' : '/updateCustomer.html';
 
-//Lisää piilokenttä id:lle
-let input = document.createElement('input');
-input.value = customer._id;
-input.type = 'hidden'
-input.name = '_id'
-form.appendChild(input);
+  //Lisää piilokenttä id:lle
+  let input = document.createElement('input');
+  input.value = customer._id;
+  input.type = 'hidden'
+  input.name = '_id'
+  form.appendChild(input);
 
-// Jos update -> lisää lomakkeelle muutkin tiedot
-// lisätään asiakkaan nimi
-input = document.createElement('input');
-input.value = customer.name;
-input.type = 'hidden' //palataan
-input.name = 'name'
-form.appendChild(input);
+  // Jos update -> lisää lomakkeelle muutkin tiedot
+  // lisätään asiakkaan nimi
+  input = document.createElement('input');
+  input.value = customer.name;
+  input.type = 'hidden' //palataan
+  input.name = 'name'
+  form.appendChild(input);
 
-// lisätään syntymäpäivä
-input = document.createElement('input');
-input.value = customer.birthday;
-input.type = 'hidden'
-input.name = 'birthday'
-form.appendChild(input);
+  // lisätään syntymäpäivä
+  input = document.createElement('input');
+  input.value = customer.birthday;
+  input.type = 'hidden'
+  input.name = 'birthday'
+  form.appendChild(input);
 
-// lisätään sähköposti
-input = document.createElement('input');
-input.value = customer.email;
-input.type = 'hidden'
-input.name = 'email'
-form.appendChild(input);
+  // lisätään sähköposti
+  input = document.createElement('input');
+  input.value = customer.email;
+  input.type = 'hidden'
+  input.name = 'email'
+  form.appendChild(input);
 
-// lisätään osoite
-input = document.createElement('input');
-input.value = customer.address;
-input.type = 'hidden'
-input.name = 'address'
-form.appendChild(input);
+  // lisätään osoite
+  input = document.createElement('input');
+  input.value = customer.address;
+  input.type = 'hidden'
+  input.name = 'address'
+  form.appendChild(input);
 
-// lisätään postinumero
-input = document.createElement('input');
-input.value = customer.postalcode;
-input.type = 'hidden'
-input.name = 'postalcode'
-form.appendChild(input);
+  // lisätään postinumero
+  input = document.createElement('input');
+  input.value = customer.postalcode;
+  input.type = 'hidden'
+  input.name = 'postalcode'
+  form.appendChild(input);
 
-// lisätään puhelinnumero
-input = document.createElement('input');
-input.value = customer.phonenumber;
-input.type = 'hidden'
-input.name = 'phonenumber'
-form.appendChild(input);
+  // lisätään puhelinnumero
+  input = document.createElement('input');
+  input.value = customer.phonenumber;
+  input.type = 'hidden'
+  input.name = 'phonenumber'
+  form.appendChild(input);
 
-// Lisää painike
-input = document.createElement('input');
-input.type = 'submit';
-input.value = (action == 'delete') ? 'Delete customer' : 'Update customer';
-form.appendChild(input)
-newCell.appendChild(form);
-return newCell;
+  // Lisää painike
+  input = document.createElement('input');
+  input.type = 'submit';
+  input.value = (action == 'delete') ? 'Delete customer' : 'Update customer';
+  form.appendChild(input)
+  newCell.appendChild(form);
+  return newCell;
 
 }
